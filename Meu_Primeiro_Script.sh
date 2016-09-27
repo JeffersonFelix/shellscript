40b97287634461e456b48b35385b52cbe55f9ae0
Eiji Kumamoto <eiji.kumamoto@gmail.com>
2016-09-27 20:03:46 -0300

    update do script

--- Meu_Primeiro_Script.sh
+++ Meu_Primeiro_Script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #Declara uma variavel e exporta para o ambiente
 #OU, coloca-se no arquivo ~/.bashrc
@@ -14,20 +14,19 @@ echo "Caminho completo para o log eh: $path_log"
 #testara se a variavel $1 está vazia, caso sim, eecutar o if
 #se  caso a variavel nao estiver, pulara o if
 if [ -z $1 ]; then
-	echo "Deve-se digitar um parametro para o Script."
-	echo "Exemplo: $0 ajuda"
+	echo "Deve-se digitar um parametro para o Script. Exemplo: $0 ajuda."
 	exit;
 fi
 
 #testara se a variavel $1 OU  se a variavel $2 foi dada entrada de texto "ajuda",
 #se sim,  ira exibir a ajuda. senao pulara o if
-if [[ "$1" = "ajuda"]] || [ "$2" = "ajuda" ]; then
-	echo "-------------------------------------------------------------------"
-	echo "Precisa de Ajuda? - Script desenvolvido por Eiji Kumamoto Neto."
-	echo "Como usar corretamente o Script."
-	echo "Voce deve indicar o diretori onde o log devera ser salvo como parametro."
-	echo "Exemplo: $0 diretorio"
-	echo "-------------------------------------------------------------------"
+if [ "$1" = "ajuda" ] || [ "$2" = "ajuda" ]; then
+	echo "---------------------------------------------------------------------------"
+	echo "- Precisa de Ajuda? - Script desenvolvido por Eiji Kumamoto Neto.         -"
+	echo "- Como usar corretamente o Script.                                        -"
+	echo "- Voce deve indicar o diretori onde o log devera ser salvo como parametro.-"
+	echo "- Exemplo: $0 diretorio                                                   -"
+	echo "---------------------------------------------------------------------------"
 	exit;
 fi
 echo "Muito bem! Voce escolheu o diretorio: $1 para salvar o LOG."
